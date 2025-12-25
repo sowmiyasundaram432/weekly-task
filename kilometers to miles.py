b9bd79541d80cb1e8c99bc768ad3fa8e57519ce8
sowmiyasundaram432 <sowmiyasundaram06@gmail.com>
2025-12-24 20:45:26 -0800

    Add files via upload

---
+++ kilometers to miles.py
@@ -0,0 +1,8 @@
+#kilometer to miles
+km=float(input("Enter the kilometer:"))
+miles=km*0.62
+print(f"the conversion of kilometer to miles is:{miles:.2f}")
+#miles to kilometer
+m=float(input("Enter the miles:"))
+kilometer=m*1.60
+print(f"the conversion of miles to kilometer is:{kilometer:.2f}")
